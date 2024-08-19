c3106c68de6bf74778b8087d38bbde505fa6838b
ier <example@mail.com>
2024-08-19 14:49:24 +0200

    chore: Remove unused environment variables and IDE configuration files

--- config/index.ts
+++ config/index.ts
@@ -4,10 +4,10 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
-  privacy_policy: '',
+  title: 'Outreach Generator',
+  description: 'The tool is designed to streamline your outreach efforts. It operates by accepting user-provided input, conducting competitive research on the specified school, evaluating the provided URL for potential marketing opportunities, and generating introductory and follow-up emails. This workflow is specifically tailored for outreach use cases.',
+  copyright: 'IER',
+  privacy_policy: 'This is a placeholder privacy policy.',
   default_language: 'en-US',
 }
 
